2307bca63240de40e3ff73e93502ecfb72bc5cde
Emma Hack <ehack78@gmail.com>
2021-01-08 15:07:24 +0000

    initial commit

---
+++ src/main/java/Deck.java
@@ -0,0 +1,16 @@
+import javax.smartcardio.Card;
+import java.util.ArrayList;
+
+public class Deck {
+
+    private ArrayList<Card> deckOfCards;
+
+    public Deck(){
+        this.deckOfCards = deckOfCards;
+    }
+
+    public int countCardsInDeck() {
+        return this.deckOfCards.size();
+    }
+}
+
